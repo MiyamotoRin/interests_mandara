9a20911510e50f63c706c571854ca44d4fd8b9c5
MiyamotoRin <muhe.iroiro.thedoctor@digitalnature.slis.tsukuba.ac.jp>
2023-10-16 20:08:28 +0900

    feat: 表示を円形っぽくしてみる

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+function arrangeKeywordsCircularly(centerElement, keywords) {
+    const radius = 150; // radius of the circle
+    const centerX = centerElement.offsetLeft + centerElement.offsetWidth / 2;
+    const centerY = centerElement.offsetTop + centerElement.offsetHeight / 2;
+
+    const angleIncrement = 2 * Math.PI / keywords.length;
+
+    keywords.forEach((keywordDiv, index) => {
+        const angle = angleIncrement * index;
+        const x = centerX + radius * Math.cos(angle) - keywordDiv.offsetWidth / 2;
+        const y = centerY + radius * Math.sin(angle) - keywordDiv.offsetHeight / 2;
+
+        keywordDiv.style.position = 'absolute';
+        keywordDiv.style.left = x + 'px';
+        keywordDiv.style.top = y + 'px';
+    });
+}
 document.getElementById('submit-button').addEventListener('click', function() {
     // キーワードの取得
     var keyword = document.getElementById('keyword-input').value;
@@ -14,7 +32,12 @@ document.getElementById('submit-button').addEventListener('click', function() {
         suggestedDiv.appendChild(keywordDiv);
     });
     
-    // TODO: 曼荼羅表示エリアの更新
+    
+    // Get the center element (the main keyword)
+    var centerElement = document.getElementById('keyword-input');
+    // Arrange the suggested keywords circularly around the main keyword
+    arrangeKeywordsCircularly(centerElement, Array.from(suggestedDiv.children));
+    
 });
 
 function mockSuggestKeywords(keyword) {
